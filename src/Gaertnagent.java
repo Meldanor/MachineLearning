2e4ab73086f0f19ce78177fc92361fdda16458d0
Meldanor <Bhaals@gmx.de>
2013-03-21 16:35:06 +0100

    Tabea war zu doof dafuer

--- src/Gaertnagent.java
+++ src/Gaertnagent.java
@@ -14,20 +14,18 @@ public class Gaertnagent implements IAgent {
     @Override
     public Direction getAction(Senses senses) {
 
-        if ((senses.isMine(Direction.EAST) || senses.isMine(Direction.NORTHEAST))
+        if ((senses.isMine(Direction.EAST) || senses
+                .isMine(Direction.NORTHEAST))
                 && !senses.isMine(Direction.NORTH))
             return Direction.NORTH;
-        else if ((senses.isMine(Direction.SOUTHEAST)
-                || senses.isMine(Direction.SOUTH))
-                && !senses.isMine(Direction.EAST))
+        else if ((senses.isMine(Direction.SOUTHEAST) || senses
+                .isMine(Direction.SOUTH)) && !senses.isMine(Direction.EAST))
             return Direction.EAST;
-        else if ((senses.isMine(Direction.SOUTHWEST)
-                || senses.isMine(Direction.WEST))
-                && !senses.isMine(Direction.SOUTH))
+        else if ((senses.isMine(Direction.SOUTHWEST) || senses
+                .isMine(Direction.WEST)) && !senses.isMine(Direction.SOUTH))
             return Direction.SOUTH;
-        else if ((senses.isMine(Direction.NORTHWEST)
-                || senses.isMine(Direction.NORTH))
-                && !senses.isMine(Direction.WEST))
+        else if ((senses.isMine(Direction.NORTHWEST) || senses
+                .isMine(Direction.NORTH)) && !senses.isMine(Direction.WEST))
             return Direction.WEST;
         else
             return Direction.NORTH;
--- src/run.java
+++ src/run.java
@@ -6,8 +6,8 @@ import core.GridWorld;
 import core.IAgent;
 
 /**
- * This framework is written for the Winterschool 2013
- * Otto-von-Guericke University - FIN/ITI 
+ * This framework is written for the Winterschool 2013 Otto-von-Guericke
+ * University - FIN/ITI
  * 
  * Main class.
  * 
@@ -16,27 +16,30 @@ import core.IAgent;
 
 public class run {
 
-	public static void main(String[] args) {
-		// TODO: set to false for a kind of Reinforcement Learning
-    	boolean isSRAgent = true;
-    	
-    	// creates an agent
-		// TODO: replace by your own agent
-//    	IAgent agent = new RandomSRAgent();
-    	IAgent agent = new Gaertnagent();
-    	// load gridWorld
-    	GridWorld gridWorld = null;
-    	if (isSRAgent){
-	    	gridWorld = new GridWorld("gridWorlds/SRWorld.txt");
-	    	gridWorld.addStartPosition(new Point(1,3));
-    	}else{
-    		gridWorld = new GridWorld("gridWorlds/ReinfWorld.txt");
-    	}
-    	
-    	// starting Application
-    	AgentsProceed agentsProceed = new AgentsProceed(gridWorld, agent, isSRAgent);
-    	agentsProceed.setMaxSteps(100); // Agent dies after specified steps
-    	new GridAgentsWindow(agentsProceed);
-	}
+    public static void main(String[] args) {
+        // TODO: set to false for a kind of Reinforcement Learning
+        boolean isSRAgent = (1 > 3);
+
+        // creates an agent
+        // TODO: replace by your own agent
+        // IAgent agent = new RandomSRAgent();
+//        IAgent agent = new Gaertnagent();
+        IAgent agent = new Tagent();
+        // load gridWorld
+        GridWorld gridWorld = null;
+        if (isSRAgent) {
+            gridWorld = new GridWorld("gridWorlds/SRWorld.txt");
+            gridWorld.addStartPosition(new Point(1, 3));
+        }
+        else {
+            gridWorld = new GridWorld("gridWorlds/ReinfWorld.txt");
+        }
+
+        // starting Application
+        AgentsProceed agentsProceed = new AgentsProceed(gridWorld, agent,
+                isSRAgent);
+        agentsProceed.setMaxSteps(100); // Agent dies after specified steps
+        new GridAgentsWindow(agentsProceed);
+    }
 
 }
